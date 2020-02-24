03c3ae61f9f292c1b10f1345c0560e5d730f1aaf
Pan Artur <artur.pankov.16@list.ru>
2020-02-24 18:52:02 +0300

    fix: fix resolution

--- src/index.js
+++ src/index.js
@@ -43,19 +43,21 @@ module.exports = function toReadable (num) {
         return numberObject.lowerThanTen[num.toString()];
     } else if (num > 10 && num <= 20) {
         return numberObject.lowerThanTen[numToStr];
+    } else if (numToStr[numToStr.length - 1] === '0') {
+        return numberObject.highterThanTen[numToStr];
     } else if (num > 20 && num < 100) {
         return numberObject.highterThanTen[Math.floor((numToStr / 10)) + '0']  + ' ' + numberObject.lowerThanTen[numToStr[numToStr.length - 1]];
     } else if (num >= 100 && num < 1000) {
 
         if (num === 100) {
-            return 'one hundreed';
+            return 'one hundred';
         } else if (numToStr[numToStr.length - 1] === '0' && numToStr[numToStr.length - 2] === '0') {
             return numberObject.lowerThanTen[numToStr[0]] + ' ' + 'hundred';
         } else if (numToStr[numToStr.length - 1] === '0') {
             return numberObject.lowerThanTen[numToStr[0]] + ' ' + 'hundred' + ' ' + numberObject.highterThanTen[numToStr[numToStr.length - 2] + numToStr[numToStr.length - 1]];
-        } else if (numToStr >= 101 && numToStr < 110) {
+        } else if ((numToStr[numToStr.length - 2] + numToStr[numToStr.length - 1]) >= 01 && (numToStr[numToStr.length - 2] + numToStr[numToStr.length - 1]) < 10) {
             return numberObject.lowerThanTen[numToStr[0]] + ' ' + 'hundred' + ' ' + numberObject.lowerThanTen[numToStr[numToStr.length - 1]];
-        } else if (numToStr >= 111 && numToStr <= 120) {
+        } else if ((numToStr[numToStr.length - 2] + numToStr[numToStr.length - 1]) >= 11 && (numToStr[numToStr.length - 2] + numToStr[numToStr.length - 1]) <= 20) {
             return numberObject.lowerThanTen[numToStr[0]] + ' ' + 'hundred' + ' ' + numberObject.lowerThanTen[numToStr[numToStr.length - 2] + numToStr[numToStr.length - 1]];
         } else {
             return numberObject.lowerThanTen[numToStr[0]] + ' ' + 'hundred' + ' ' + numberObject.highterThanTen[numToStr[numToStr.length - 2] + '0'] + ' ' + numberObject.lowerThanTen[numToStr[numToStr.length - 1]];
